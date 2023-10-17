184b70b0730b336b8f0587bdec5a0982ad259235
danmakowiesky <dvm1993@hotmail.com>
2023-10-16 21:01:50 -0300

    eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,5 @@
-import "dotenv/config";
-import {z} from "zod"
+import 'dotenv/config'
+import {z} from 'zod'
 
 export const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
@@ -7,7 +7,7 @@ export const envSchema = z.object({
 })
 const _env = envSchema.safeParse(process.env)
 if(_env.success === false) {
-   console.error('⚠️  Invalid enviroment valiable', _env.error.format())
+  console.error('⚠️  Invalid enviroment valiable', _env.error.format())
   throw new Error('invalid enviremoment variable')
 }
-export const env = _env.data;
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import fastify from "fastify"
-import { env } from "./env";
+import fastify from 'fastify'
+import { env } from './env'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/', (request, reply) => {
-  return reply.status(200).send({message: "hello world"});
+  return reply.status(200).send({message: 'hello world'})
 })
 
-app.listen({port: env.PORT , host: '0.0.0.0'}).then(() => console.log('🚀, server is running, on port 3333'));
+app.listen({port: env.PORT , host: '0.0.0.0'}).then(() => console.log('🚀, server is running, on port 3333'))
